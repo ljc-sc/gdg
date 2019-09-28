fa50569bf4fe7ba9c3ba4c483fdd87f0ef4e1185
lin <ljc@qq.com>
2019-09-28 15:53:55 +0800

    Initial commit

---
+++ src/main/java/com/example/gdg/GdgApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdgApplication.class, args);
+    }
+
+}
